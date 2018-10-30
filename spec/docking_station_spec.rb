8f96ea8e3c8dbb728ce6e7eb33ed52aa04ed306c
cesare camurani <cesare.camurani@gmail.com>
2018-10-30 18:03:46 +0000

    raise error commit

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,13 @@ require "docking_station"
 describe DockingStation do
   it { expect(DockingStation.new.release_bike) }
   it { expect(DockingStation.new.dock_bike) }
+
+  describe "release_bike" do
+    it {expect { DockingStation.new.release_bike }.to raise_error "Station is empty"}
+  end
 end
+
+
 
 describe Bike do
   it { expect(Bike.new.working?) }
